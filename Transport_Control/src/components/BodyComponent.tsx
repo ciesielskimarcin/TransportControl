fe59354a8abc46229e148463ff8b552e70b71870
Marcin Ciesielski <marcin.ciesielski@edrmedeso.com>
2025-03-21 12:45:03 +0100

    TransportCategories with comments from Sondre

---
+++ Transport_Control/src/components/BodyComponent.tsx
@@ -0,0 +1,64 @@
+import { ModusTreeView, ModusTreeViewItem } from "@trimble-oss/modus-react-components"
+import { useState } from "react";
+import { TransportType } from "../Enums/TransportType";
+import { TransportTypeEntity } from "../Entities/TransportTypeEntity";
+import ActionBar from './ActionBar'
+import TransportCategoriesList from "./TransportCategoriesList";
+import _ from "lodash";
+
+const initialState = [{
+    name: "Standard",
+    checkOrder: 1,
+    type: TransportType.Standard,
+    transportFirstDimension: 2438.4,
+    transportSecondDimension: 2590.8,
+    transportThirdDimension: 16154.4,
+    weight: 151,
+    elements: [],
+},
+{
+    name: "withOneEscort",
+    checkOrder: 2,
+    type: TransportType.Standard,
+    transportFirstDimension: 2438.4,
+    transportSecondDimension: 4343.4,
+    transportThirdDimension: 16154.4,
+    weight: 22500,
+    elements: [],
+},
+{
+    name: "withTwoEscort",
+    checkOrder: 3,
+    type: TransportType.Standard,
+    transportFirstDimension: 2438.4,
+    transportSecondDimension: 4978.4,
+    transportThirdDimension: 16154.4,
+    weight: 40823.31,
+    elements: [],
+},
+{
+    name: "NonStandard",
+    checkOrder: 99,
+    type: TransportType.NonStandard,
+    transportFirstDimension: Number.MAX_VALUE,
+    transportSecondDimension: Number.MAX_VALUE,
+    transportThirdDimension: Number.MAX_VALUE,
+    weight: Number.MAX_VALUE,
+    elements: [],
+},]
+
+export default function BodyComponent(){
+
+    const [transports, setTransports] = useState<TransportTypeEntity[]>(initialState)
+
+    return (
+        <>
+          <div>
+            <ActionBar />
+            <TransportCategoriesList/>
+            {/* <CheckTransport/> */}
+          </div>
+        </>
+      )
+    
+}
--- Transport_Control/src/components/TransportCategoriesList.tsx
+++ Transport_Control/src/components/TransportCategoriesList.tsx
@@ -4,7 +4,51 @@ import { TransportType } from "../Enums/TransportType";
 import { TransportTypeEntity } from "../Entities/TransportTypeEntity";
 import _ from "lodash";
 
+const initialState = [{
+    name: "Standard",
+    checkOrder: 1,
+    type: TransportType.Standard,
+    transportFirstDimension: 2438.4,
+    transportSecondDimension: 2590.8,
+    transportThirdDimension: 16154.4,
+    weight: 151,
+    elements: [],
+},
+{
+    name: "withOneEscort",
+    checkOrder: 2,
+    type: TransportType.Standard,
+    transportFirstDimension: 2438.4,
+    transportSecondDimension: 4343.4,
+    transportThirdDimension: 16154.4,
+    weight: 22500,
+    elements: [],
+},
+{
+    name: "withTwoEscort",
+    checkOrder: 3,
+    type: TransportType.Standard,
+    transportFirstDimension: 2438.4,
+    transportSecondDimension: 4978.4,
+    transportThirdDimension: 16154.4,
+    weight: 40823.31,
+    elements: [],
+},
+{
+    name: "NonStandard",
+    checkOrder: 99,
+    type: TransportType.NonStandard,
+    transportFirstDimension: Number.MAX_VALUE,
+    transportSecondDimension: Number.MAX_VALUE,
+    transportThirdDimension: Number.MAX_VALUE,
+    weight: Number.MAX_VALUE,
+    elements: [],
+},]
+
+
 export default function TransportCategoriesList() {
+
+    const [transports, setTransports] = useState<TransportTypeEntity[]>(initialState)
 
 
     const [transportName, setTransportName] = useState("");
@@ -13,52 +57,8 @@ export default function TransportCategoriesList() {
         setTransportName(id);
         console.log("tranportname:", id);
     };
-    
 
-    const [transports, setTransports] = useState<TransportTypeEntity[]>([
-            {
-                name: "Standard",
-                checkOrder: 1,
-                type: TransportType.Standard,
-                transportFirstDimension: 2438.4,
-                transportSecondDimension: 2590.8,
-                transportThirdDimension: 16154.4,
-                weight: 151,
-                elements: [],
-            },
-            {
-                name: "withOneEscort",
-                checkOrder: 2,
-                type: TransportType.Standard,
-                transportFirstDimension: 2438.4,
-                transportSecondDimension: 4343.4,
-                transportThirdDimension: 16154.4,
-                weight: 22500,
-                elements: [],
-            },
-            {
-                name: "withTwoEscort",
-                checkOrder: 3,
-                type: TransportType.Standard,
-                transportFirstDimension: 2438.4,
-                transportSecondDimension: 4978.4,
-                transportThirdDimension: 16154.4,
-                weight: 40823.31,
-                elements: [],
-            },
-            {
-                name: "NonStandard",
-                checkOrder: 99,
-                type: TransportType.NonStandard,
-                transportFirstDimension: Number.MAX_VALUE,
-                transportSecondDimension: Number.MAX_VALUE,
-                transportThirdDimension: Number.MAX_VALUE,
-                weight: Number.MAX_VALUE,
-                elements: [],
-            },
-        ]);
 
-        
 
     return (
         <div>
